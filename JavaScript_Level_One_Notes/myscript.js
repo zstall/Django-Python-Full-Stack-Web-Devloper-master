6a152f1de2709f72644e465df5e3a990af6c92b3
zstall <zstall4@gmail.com>
2018-08-08 11:46:57 -0600

    Sompleted Bootstrap starting Javascript

---
+++ JavaScript_Level_One_Notes/myscript.js
@@ -0,0 +1,5 @@
+alert("Welcome to your Bank!")
+
+var deposit = prompt("How much would like to deposit? ");
+alert("You've deposited: "+deposit);
+console.log("Your are great!");
---
+++ JavaScript_Level_One_Notes/zspart3.js
@@ -0,0 +1,4 @@
+var weight = prompt("Enter your weight in pounds: ")
+var newWeight = weight * 0.454
+alert("Your weight in kilograms is: " +newWeight)
+console.log("Conversion complete!")
