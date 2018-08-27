93d20490972daa41dd0ab23495c5967c2f0d9b26
zstall <zstall4@gmail.com>
2018-08-27 16:14:24 -0600

    Finish Django #5

--- Django_Level_Five_Notes/learning_users/basic_app/views.py
+++ Django_Level_Five_Notes/learning_users/basic_app/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from basic_app.forms import UserForm, UserProfileInfoForm
 # Create your views here.
 
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+
+
 
 def index(request):
     return render(request, 'basic_app/index.html')
+
+@login_required
+def special(request):
+    return HttpResponse("You are logged in, Nice!")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
 
 def register(request):
     registered = False
@@ -36,3 +53,27 @@ def register(request):
                                             {   'user_form':user_form,
                                                 'profile_form':profile_form,
                                                 'registered':registered})
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+
+            else:
+                return HttpResponse("Account Not Active")
+
+        else:
+            print("Someone tried to login and failed")
+            print("Username: {} and password {}".format(username,password))
+            return HttpResponse("Invalid Login Details Supplied")
+
+    else:
+        return render(request,'basic_app/login.html',{})
--- Django_Level_Five_Notes/learning_users/learning_users/settings.py
+++ Django_Level_Five_Notes/learning_users/learning_users/settings.py
@@ -137,3 +137,6 @@ STATICFILES_DIRS = [STATIC_DIR,]
 # Media
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+
+LOGIN_URL = '/basic_app/user_login'
