3ab348a1296f84c2cecb7a605cb33c8153b2dc6b
Zachary Stall <zstall4@gmail.com>
2018-09-12 17:35:22 -0600

    work from home

--- zs_blog_project/zs_blog/blog/models.py
+++ zs_blog_project/zs_blog/blog/models.py
@@ -29,7 +29,7 @@ class Comment(models.Model):
     post = models.ForeignKey('blog.Post',related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
